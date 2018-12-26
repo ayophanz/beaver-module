74705fafd71ced2ee6175bc8dbd83688fa126c2a
ayophanz <ayophanz18@gmail.com>
2018-12-27 06:46:58 +0800

    Add files via upload

---
+++ modules/theme/woocommerce-featured/includes/frontend.php
@@ -0,0 +1,63 @@
+<section class="<?php echo $module->get_classname(); ?>">
+	<div class="woocommerce-featured-inner woocommerce">
+		<div class="products owl-carousel">
+			<?php if ( class_exists( 'WooCommerce' ) ) {
+				$args = array(
+					'post_type' => 'product',
+					'meta_key' => '_featured',
+					'meta_value' => 'yes',
+					'posts_per_page' => $settings->total_item,
+					'orderby' => $settings->orderby,
+					'order' => $settings->order,
+					);
+				$loop = new WP_Query( $args );
+				if ( $loop->have_posts() ) {
+					while ( $loop->have_posts() ) : $loop->the_post();?>
+							<div <?php post_class();?>>
+								<?php //source = woo/templates/content-product.php
+								/**
+								 * woocommerce_before_shop_loop_item hook.
+								 *
+								 * @hooked woocommerce_template_loop_product_link_open - 10
+								 */
+								do_action( 'woocommerce_before_shop_loop_item' );
+								/**
+								 * woocommerce_before_shop_loop_item_title hook.
+								 *
+								 * @hooked woocommerce_show_product_loop_sale_flash - 10
+								 * @hooked woocommerce_template_loop_product_thumbnail - 10
+								 */
+								do_action( 'woocommerce_before_shop_loop_item_title' );
+								/**
+								 * woocommerce_shop_loop_item_title hook.
+								 *
+								 * @hooked woocommerce_template_loop_product_title - 10
+								 */
+								do_action( 'woocommerce_shop_loop_item_title' );
+								/**
+								 * woocommerce_after_shop_loop_item_title hook.
+								 *
+								 * @hooked woocommerce_template_loop_rating - 5
+								 * @hooked woocommerce_template_loop_price - 10
+								 */
+								do_action( 'woocommerce_after_shop_loop_item_title' );
+								/**
+								 * woocommerce_after_shop_loop_item hook.
+								 *
+								 * @hooked woocommerce_template_loop_product_link_close - 5
+								 * @hooked woocommerce_template_loop_add_to_cart - 10
+								 */
+								do_action( 'woocommerce_after_shop_loop_item' ); ?>
+							</div>
+					<?php endwhile;
+				} else {
+					echo '<h6 class="no-found">';
+					echo __( 'No products found' );
+					echo '</h6>';
+				}
+			wp_reset_postdata();
+			} ?>
+		</div>
+	</div>
+</section>
+
---
+++ modules/theme/woocommerce-featured/woocommerce-featured.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * @class WooCommerceFeaturedModule
+ */
+class WooCommerceFeaturedModule extends FLBuilderModule {
+
+	/**
+	 * @method __construct
+	 */
+	public function __construct()
+	{
+		$enabled = class_exists('Woocommerce');
+		global $foldercategory;
+		parent::__construct(array(
+			'name'          	=> __('Woocommerce - Featured', 'fl-builder'),
+			'description'   	=> __('Display Shop Product.', 'fl-builder'),
+			'category'      	=> __($foldercategory, 'fl-builder'),
+			'enabled'       	=> $enabled,
+			'partial_refresh'	=> true
+		));
+		// Register and enqueue your own.
+		$this->add_css( 'owl-carousel', FL_MODULE_THEME_URL . 'assets/owlcarousel/owl.carousel.css' );
+		$this->add_css( 'owl-carousel-theme', FL_MODULE_THEME_URL . 'assets/owlcarousel/owl.theme.default.min.css' );
+		$this->add_js( 'owl-carousel', FL_MODULE_THEME_URL . 'assets/owlcarousel/owl.carousel.min.js', array(), '', true );
+	}
+
+	/**
+	 * @method get_classname
+	 */
+	public function get_classname()
+	{
+		$classname = 'woocommerce-featured';
+		return $classname;
+	}
+}
+
+/**
+ * Register the module and its form settings.
+ */
+FLBuilder::register_module('WooCommerceFeaturedModule', array(
+	'general'       => array(
+		'title'         => __('General', 'fl-builder'),
+		'sections'      => array(
+			'general'         => array(
+				'title'         => '',
+				'fields'        => array(
+					'total_item'           => array(
+						'type'          => 'text',
+						'label'         => __( 'Total Item', 'fl-builder' ),
+						'default'       => '12',
+						'maxlength'     => '3',
+						'size'          => '4',
+						'description'   => 'Number',
+						'placeholder'   => '3'
+					),
+					'orderby'       => array(
+						'type'          => 'select',
+						'label'         => __('Sort By', 'fl-builder'),
+						'default'       => 'date',
+						'options'       => array(
+							'menu_order'    => _x( 'Default', 'Sort by.', 'fl-builder' ),
+							'popularity'    => __('Popularity', 'fl-builder'),
+							'rating'        => __('Rating', 'fl-builder'),
+							'date'          => __('Date', 'fl-builder'),
+							'price'         => __('Price', 'fl-builder')
+						)
+					),
+					'order'         => array(
+						'type'          => 'select',
+						'label'         => __('Sort Direction', 'fl-builder'),
+						'default'       => 'DESC',
+						'options'       => array(
+							'ASC'           => __('Ascending', 'fl-builder'),
+							'DESC'          => __('Descending', 'fl-builder')
+						)
+					),
+					'column_items'         => array(
+						'type'          => 'select',
+						'label'         => __('Number of Column', 'fl-builder'),
+						'default'       => '3',
+						'options'       => array(
+							'1'    		=> __('1', 'fl-builder'),
+							'2'    		=> __('2', 'fl-builder'),
+							'3'    		=> __('3', 'fl-builder'),
+							'4'    		=> __('4', 'fl-builder'),
+							'5'    		=> __('5', 'fl-builder'),
+							'6'    		=> __('6', 'fl-builder'),
+						)
+					),
+					'margin'         => array(
+						'type'          => 'text',
+						'label'         => __('Column Margin', 'fl-builder'),
+						'default'       => __('30', 'fl-builder'),
+						'placeholder'       => __('30', 'fl-builder'),
+						'maxlength'     => '3',
+						'size'          => '2',
+					),
+					'autoplay'        => array(
+						'type'          => 'select',
+						'label'         => __('Autoplay', 'fl-builder'),
+						'default'       => 'Yes',
+						'options'       => array(
+							'true'   	=> __('Yes', 'fl-builder'),
+							'false'     => __('No', 'fl-builder'),
+						)
+					),
+					'dots'        => array(
+						'type'          => 'select',
+						'label'         => __('Enable Pagination', 'fl-builder'),
+						'default'       => 'No',
+						'options'       => array(
+							'true'   	=> __('Yes', 'fl-builder'),
+							'false'     => __('No', 'fl-builder'),
+						)
+					),
+					'nav'        => array(
+						'type'          => 'select',
+						'label'         => __('Enable Navigation', 'fl-builder'),
+						'default'       => 'No',
+						'options'       => array(
+							'true'   	=> __('Yes', 'fl-builder'),
+							'false'     => __('No', 'fl-builder'),
+						)
+					),
+				)
+			)
+		),
+	),
+));
