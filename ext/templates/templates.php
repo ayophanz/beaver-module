aa08310cbcbe3e157c5b70e428451305bc3114c5
ayophanz <ayophanz18@gmail.com>
2018-12-27 06:39:46 +0800

    Add files via upload

---
+++ ext/templates/templates.php
@@ -0,0 +1,8 @@
+<?php
+
+define( 'FL_BUILDER_USER_TEMPLATES_DIR', FL_MODULE_THEME_DIR . 'ext/templates/' );
+define( 'FL_BUILDER_USER_TEMPLATES_URL', FL_MODULE_THEME_DIR . 'ext/templates/' );
+
+// Classes
+require_once FL_BUILDER_USER_TEMPLATES_DIR . 'classes/templates.php';
+require_once FL_BUILDER_USER_TEMPLATES_DIR . 'classes/templates-admin.php';
